22890206ba950b90b041a9835cfe01245678d5a1
Federico Brigante <bfred-it@users.noreply.github.com>
2016-09-16 15:29:51 +0700

    xo fixes and simplification

--- index.js
+++ index.js
@@ -4,24 +4,18 @@ import once from 'one-event';
 const attach = 'addEventListener';
 const remove = 'removeEventListener';
 
-const canControlScrollRestoration = 'scrollRestoration' in window.history;
 let isPrevented = false;
-
-/**
- * Scroll functions
- */
 let lastScrollPosition;
+
 function resetScroll() {
 	window.scrollTo.apply(window, lastScrollPosition);
 }
+
 function waitForScroll() {
 	lastScrollPosition = [getScrollLeft(), getScrollTop()];
 	once(window, 'scroll', resetScroll);
 }
 
-/**
- * Toggle functions
- */
 function event(action) {
 	// run "off" only if it's prevented
 	// otherwise run "off" only if it's not already prevented
@@ -29,16 +23,8 @@ function event(action) {
 		window[action]('popstate', waitForScroll);
 	}
 }
-function allow() {
-	event(remove);
-	isPrevented = false;
-}
-function prevent() {
-	event(attach);
-	isPrevented = true;
-}
 
-function init() {
+if (!('scrollRestoration' in window.history)) {
 	Object.defineProperty(history, 'scrollRestoration', {
 		enumerable: true,
 		get() {
@@ -46,13 +32,16 @@ function init() {
 		},
 		set(ScrollRestoration) {
 			switch (ScrollRestoration) {
-				case 'auto': allow(); break;
-				case 'manual': prevent(); break;
+				case 'auto':
+					event(remove);
+					isPrevented = false;
+					break;
+				case 'manual':
+					event(attach);
+					isPrevented = true;
+					break;
+				default:
 			}
 		}
 	});
-}
-
-if (!canControlScrollRestoration) {
-	init();
 }
